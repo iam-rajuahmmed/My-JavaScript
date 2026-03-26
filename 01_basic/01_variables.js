bf512741c89325047674039143841166cb3ed384
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-03-26 13:37:19 +0000

    variables & datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 33
+let accoutEmail = 'raju@gmail.com'
+var accPass = "123456"
+
+myID = "Raju011"
+console.table([accountId, accoutEmail, accPass, myID])
+ 
+// accountId = 45 // Not Allowed becasue this is const veriables calledcls
+accoutEmail = 'sagor@gmail.com'
+accPass = 'sagor01'
+myID = 10
+console.table([accountId, accoutEmail, accPass, myID])
+
+
+
+console.log(myID);
+console.log(myID);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+let name = ' Raju Ahmmed' // this is string
+
+let Age = 28 // this is number
+
+let isRight = true // this is boolean true or false
+
+let tamprature = null // this is null but datatype object
+
+let state; // this is undifined and also datatype also Undifined
+
+
+
+
+console.log(typeof name);
+console.log(typeof Age);
+console.log(typeof isRight);
+console.log(typeof tamprature);
+console.log(typeof state);
+
