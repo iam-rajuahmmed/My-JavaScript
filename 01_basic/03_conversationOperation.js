e10f605367ab1a5dc97b4cab871e66902db42d57
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-03-26 14:13:27 +0000

    comperasion

--- 01_basic/03_conversationOperation.js
+++ 01_basic/03_conversationOperation.js
@@ -1,16 +1,51 @@
 let Score = "33";
 let Score2 = "33a";
 
-console.log(Score);
-console.log(typeof Score);
-console.log(typeof (Score));
+// console.log(Score);
+// console.log(typeof Score);
+// console.log(typeof (Score));
 
 // Convart
 
 let myNum = Number(Score)
 let myNum2 = Number(Score2)
 
-console.log( typeof myNum);
-console.log( typeof myNum2);
+// console.log( typeof myNum);
+// console.log( typeof myNum2);
+
+// --------------Oparattion-----------------
+
+let a = 20
+let b = 20
+
+let negValue = -b
+console.log(negValue);
+
+let total = a + b
+
+let c = negValue
+
+let ganTotal = a+b+c
+
+console.log(total);
+console.log(ganTotal);
+
+console.log( "1"+2);
+
+console.log(1 + '2');
+
+console.log(1 + 2 + '2');
+
+console.log("1" + 2 +2);
+
+
+let numm = 100
+
+numm++
+
+console.log(numm);
+
+
+
 
 
---
+++ 01_basic/04_commerision.js
@@ -0,0 +1,8 @@
+console.log('2' > 1);
+
+console.log("02"> 1);
+
+console.log('hello');
+
+console.log("10"==10);
+console.log("10"===10);
