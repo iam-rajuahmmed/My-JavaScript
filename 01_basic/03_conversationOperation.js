8b32959a2c2f479e848831090a34928421379e9b
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-03-26 13:49:06 +0000

    Conversation of data type

---
+++ 01_basic/03_conversationOperation.js
@@ -0,0 +1,16 @@
+let Score = "33";
+let Score2 = "33a";
+
+console.log(Score);
+console.log(typeof Score);
+console.log(typeof (Score));
+
+// Convart
+
+let myNum = Number(Score)
+let myNum2 = Number(Score2)
+
+console.log( typeof myNum);
+console.log( typeof myNum2);
+
+
