c18ba2d65d72daebb1c7efe2e3ba8fd78f8181f4
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-03-28 10:12:02 +0000

    basic array 01

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -1,3 +1,46 @@
 const myAry = [1, 2, 3, 4, 5]
 
 console.log(myAry[0]);
+
+myAry.push(6, 7, 8)
+
+console.log(myAry);
+myAry.pop()
+console.log(myAry);
+myAry.unshift(0, 1, 2)
+console.log(myAry);
+myAry.shift()
+console.log(myAry);
+
+console.log(myAry.includes(3));
+console.log(myAry.includes(10));
+
+console.log(myAry.indexOf(2));
+
+const newArr = ["Hello", "Medam", "jii"]
+
+const onenew = newArr.join();
+console.log(typeof onenew);
+
+// Slice and Splice
+
+const myArr = [1, 2, 3, 4, 5]
+
+console.log("A", myArr);
+
+const myarr1 = myArr.slice(1, 3)
+console.log("B", myarr1);
+
+const myarr2 = myArr.splice(1, 3)
+console.log(myArr);
+
+console.log("C", myarr2);
+
+
+
+
+
+
+
+
+
