537ee1fe42c5be4da2245ecdc5350af07c6d19aa
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-04-01 11:25:04 +0000

    If Statment

---
+++ 04_control_flow/one_basic.js
@@ -0,0 +1,12 @@
+// IF Steatment 
+// Conditonal Keys = < , >, <=, >=, ==, !=, ===, !==
+// Olny True Conditioan Exucute here 
+let data = 20;
+if(data > 10){
+    console.log("Your data is geater then 10");
+    
+}else{
+    console.log("Your Data is less then 10");
+    
+}
+
