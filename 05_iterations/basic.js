f4f11bd34184d033d3eb8278a117098c8a816151
Raju Ahmmed <69457682+iam-rajuahmmed@users.noreply.github.com>
2026-04-05 05:47:45 +0000

    loop basic

--- 05_iterations/basic.js
+++ 05_iterations/basic.js
@@ -1,6 +1,19 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
-    console.log("My Number is: "+ element);
+    // console.log("My Number is: "+ element);
     
+    
+}
+
+let myArr =  [1, 2, 3, 4, 5]
+
+for(let i = 0; i <= myArr.length; i++){
+    if (i == 4) {
+        console.log("we got number of 4 in this array");
+        break
+        
+        
+    }
+    console.log(i);
     
 }
